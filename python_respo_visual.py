002d391ecd5d5ca21476f4566cc88b6dc0f28b7a
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-04-28 18:09:46 +1200

    API and Visualization

--- python_respo_visual.py
+++ python_respo_visual.py
@@ -1,4 +1,4 @@
-from plotly.graph_objs import Bar
+from plotly.graph_objs import Bar, Layout
 from plotly import offline
 import requests
 
@@ -6,4 +6,25 @@ url = "https://api.github.com/search/repositories?q=language:python&sort=stars"
 headers = {'Accept': 'application/vnd.github.v3+json'}
 r = requests.get(url, headers = headers)
 print(f"Status code: {r.status_code}")
+
+response = r.json()
+repo_dicts = response['items']
+
+repo_names, stars = [], []
+for repo_dict in repo_dicts:
+    repo_names.append(repo_dict["name"])
+    stars.append(repo_dict["stargazers_count"])
+    
+#Make Visualization
+data = [{
+    "type" : "bar",
+    "x" : repo_names,
+    "y" : stars
+}]
+
+my_layout = {"title" : "Python Repositories", "xaxis" : {"title" : "Repository"}, "yaxis" : {"title": "Rank"}}
+
+fig = {"data" : data, "layout" : my_layout}
+offline.plot(fig, filename = "python_respos.html" )
+
 
